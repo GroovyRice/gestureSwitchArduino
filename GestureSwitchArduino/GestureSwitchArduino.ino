8174bca8d3520e99bed246dc800cd04b0ad4b2f1
Rhys Blacklidge <60952030+GroovyRice@users.noreply.github.com>
2021-08-19 20:12:22 +1000

    Update GestureSwitchArduino.ino

--- GestureSwitchArduino/GestureSwitchArduino.ino
+++ GestureSwitchArduino/GestureSwitchArduino.ino
@@ -237,22 +237,22 @@ void getGesture(String* value,int* num) {
   switch (data) {                 // When different gestures be detected, the variable 'data' will be set to different values by paj7620ReadReg(0x43, 1, &data).
     case GES_RIGHT_FLAG:
       *value = "Right";
-      if(getSwipe("Right")=="none") {return;}
+      if(getSwipe("Right")=="None") {return;}
       *num = 1;
       break;
     case GES_LEFT_FLAG:
       *value = "Left";
-      if(getSwipe("Left")=="none") {return;}
+      if(getSwipe("Left")=="None") {return;}
       *num = 1;
       break;
     case GES_UP_FLAG:
       *value = "Up";
-      if(getSwipe("Up")=="none") {return;}
+      if(getSwipe("Up")=="None") {return;}
       *num = 1;
       break;
     case GES_DOWN_FLAG:
       *value = "Down";
-      if(getSwipe("Down")=="none") {return;}
+      if(getSwipe("Down")=="None") {return;}
       *num = 1;
       break;
 //    case GES_FORWARD_FLAG:
