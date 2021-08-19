fc576a8515d127969c0b243743b7e8ac8b7b9aa7
Rhys Blacklidge <60952030+GroovyRice@users.noreply.github.com>
2021-08-19 12:15:56 +1000

    Update GestureSwitchArduino.ino

--- GestureSwitchArduino/GestureSwitchArduino.ino
+++ GestureSwitchArduino/GestureSwitchArduino.ino
@@ -199,6 +199,7 @@ void alterPoles() {
 }
 
 int getAlterPoles(String path) {
+  bool temp;
   if (Firebase.getInt(firebaseData, "/alterPoles/pole" + path)) {
     if (firebaseData.dataType() == "boolean") {
       temp = firebaseData.boolData();
@@ -223,8 +224,6 @@ String findPole(int pole) {
     return "Left";
   } else if (pole == getSwipe("swipeRightNum")) {
     return "Right";
-  } else if (){
-    return "";
   } else {
     return "None"
   }
