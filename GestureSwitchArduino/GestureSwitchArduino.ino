07a46972a7da8373e26b75bb89d4cace963a4b33
Rhys Blacklidge <60952030+GroovyRice@users.noreply.github.com>
2021-08-19 12:35:32 +1000

    Update GestureSwitchArduino.ino

--- GestureSwitchArduino/GestureSwitchArduino.ino
+++ GestureSwitchArduino/GestureSwitchArduino.ino
@@ -9,16 +9,6 @@
 
 
 FirebaseData firebaseData;
-
-//REAL TIME CLOCK
-const int GMT = +10; //change this to adapt it to your time zone
-const int myClock = 12;  // can be 24 or 12 hour clock
-const int dateOrder = 0;  // 1 = MDY; 0 for DMY
-
-RTCZero rtc; // create instance of real time clock
-int status = WL_IDLE_STATUS;
-int myhours, mins, secs, myday, mymonth, myyear;
-bool IsPM = false;
 /*
   Notice: When you want to recognize the Forward/Backward gestures, your gestures' reaction time must less than GES_ENTRY_TIME(0.8s).
         You also can adjust the reaction time according to the actual circumstance.
@@ -53,10 +43,6 @@ void setup() {
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH, WIFI_SSID, WIFI_PASSWORD);
   Firebase.reconnectWiFi(true);
 
-  rtc.begin();
-  setRTC();  // get Epoch time from Internet Time Service
-  fixTimeZone();
-
   setPoles(0,"Up");
   setPoles(0,"Down");
   setPoles(0,"Left");
@@ -75,23 +61,13 @@ void setup() {
   }
 
 void loop() {
+  //MAKE getGesture() to return a number
   String gesture = getGesture();
   if(gesture != "unknown") {
     doGesture(gesture);
   }
   delay(100);
-  alterPoles()
-  
-  secs = rtc.getSeconds();
-
-  //MAIN CLOCK LOOP
-  if (secs == 0) fixTimeZone(); // when secs is 0, update everything and correct for time zone
-  // otherwise everything else stays the same.
-  printDate();
-  printTime();
-  Serial.println();
-  while (secs == rtc.getSeconds())delay(10); // wait until seconds change
-  if (mins==59 && secs ==0) setRTC(); // get NTP time every hour at minute 59
+  alterPoles();
 }
 
 void doGesture(String temp) {
@@ -182,7 +158,7 @@ int getSwipe(String path) {
     Serial.println(firebaseData.errorReason());
   }
 }
-
+/*
 void alterPoles() {
   int poleOne, poleTwo, poleThree, poleFour;
   String strOne, strTwo, strThree, strFour;
@@ -232,7 +208,7 @@ String findPole(int pole) {
     return "None";
   }
 }
-
+*/
 String getGesture() {
   uint8_t data = 0;
   uint8_t data1 = 0;
@@ -279,105 +255,4 @@ String getGesture() {
   }
   delay(GES_QUIT_TIME);
   return value;
-}
-
-//CLOCK FUNCTIONS
-void printDate()
-{
-  if (dateOrder == 0) {
-    Serial.print(myday);
-    Serial.print("/");
-  }
-  Serial.print(mymonth);
-  Serial.print("/");
-  if (dateOrder == 1) {
-    Serial.print(myday);
-    Serial.print("/");
-  }
-  Serial.print("20");
-  Serial.print(myyear);
-  Serial.print(" ");
-}
-
-void printTime()
-{
-  print2digits(myhours);
-  Serial.print(":");
-  print2digits(mins);
-  Serial.print(":");
-  print2digits(secs);
-  if (myClock==12) {
-    if(IsPM) Serial.print("  PM");
-    else Serial.print("  AM");
-  }
-  Serial.println();
-}
-
-void print2digits(int number) {
-  if (number < 10) {
-    Serial.print("0");
-  }
-  Serial.print(number);
-}
-
-void setRTC() { // get the time from Internet Time Service
-  unsigned long epoch;
-  int numberOfTries = 0, maxTries = 6;
-  do {
-    epoch = WiFi.getTime(); // The RTC is set to GMT or 0 Time Zone and stays at GMT.
-    numberOfTries++;
-  }
-  while ((epoch == 0) && (numberOfTries < maxTries));
-
-  if (numberOfTries == maxTries) {
-    Serial.print("NTP unreachable!!");
-    while (1);  // hang
-  }
-  else {
-    Serial.print("Epoch Time = ");
-    Serial.println(epoch);
-    rtc.setEpoch(epoch);
-    Serial.println();
-  }
-}
-
-/* There is more to adjusting for time zone that just changing the hour.
-   Sometimes it changes the day, which sometimes chnages the month, which
-   requires knowing how many days are in each month, which is different
-   in leap years, and on Near Year's Eve, it can even change the year! */
-void fixTimeZone() {
-  int daysMon[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-  if (myyear % 4 == 0) daysMon[2] = 29; // fix for leap year
-  myhours = rtc.getHours();
-  mins = rtc.getMinutes();
-  myday = rtc.getDay();
-  mymonth = rtc.getMonth();
-  myyear = rtc.getYear();
-  myhours +=  GMT; // initial time zone change is here
-  if (myhours < 0) {  // if hours rolls negative
-    myhours += 24; // keep in range of 0-23
-    myday--;  // fix the day
-    if (myday < 1) {  // fix the month if necessary
-      mymonth--;
-      if (mymonth == 0) mymonth = 12;
-      myday = daysMon[mymonth];
-      if (mymonth == 12) myyear--; // fix the year if necessary
-    }
-  }
-  if (myhours > 23) {  // if hours rolls over 23
-    myhours -= 24; // keep in range of 0-23
-    myday++; // fix the day
-    if (myday > daysMon[mymonth]) {  // fix the month if necessary
-      mymonth++;
-      if (mymonth > 12) mymonth = 1;
-      myday = 1;
-      if (mymonth == 1)myyear++; // fix the year if necessary
-    }
-  }
-  if (myClock == 12) {  // this is for 12 hour clock
-    IsPM = false;
-    if (myhours > 11)IsPM = true;
-    myhours = myhours % 12; // convert to 12 hour clock
-    if (myhours == 0) myhours = 12;  // show noon or midnight as 12
-  }
 }
